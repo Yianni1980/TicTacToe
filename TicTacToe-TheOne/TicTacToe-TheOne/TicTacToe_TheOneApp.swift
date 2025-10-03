c927de2354a434a151fd3c35bae4912d60978917
Yianni1980 <88034690+Yianni1980@users.noreply.github.com>
2025-10-03 13:11:47 +0300

    Created Xcode Project

---
+++ TicTacToe-TheOne/TicTacToe-TheOne/TicTacToe_TheOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_TheOneApp.swift
+//  TicTacToe-TheOne
+//
+//  Created by ioannis on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_TheOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
